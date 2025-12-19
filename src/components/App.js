f18e95e0e4545438ff3b5f15e79cac5a4e571d09
VENKATA_REDDY <venkatareddypadala1637@gmail.com>
2025-12-19 15:25:25 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useRef, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [todo, setTodo] = useState([]);
+  const inputEl = useRef(null);
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>To-Do List</p>
+      <input type="text" ref={inputEl} />
+      <button
+        onClick={() => {
+          setTodo((cur) => [
+            ...cur,
+            { id: crypto.randomUUID(), data: inputEl.current.value },
+          ]);
+        }}
+      >
+        Add Todo
+      </button>
+      <ul>
+        {todo.map((todoItem) => {
+          return (
+            <li key={todoItem.id}>
+              <p>{todoItem.data}</p>
+              <button
+                onClick={() => {
+                  const newData = todo.filter(
+                    (item) => item.id !== todoItem.id
+                  );
+                  setTodo(newData);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
